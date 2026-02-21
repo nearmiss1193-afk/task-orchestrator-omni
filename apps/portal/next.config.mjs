4c0c665352db8c9d14aec50432d7e2092107ced5
nearmiss1193-afk <nearmiss1193-afk@users.noreply.github.com>
2026-02-20 20:12:34 -0500

    chore(config): force nextjs to rewrite static dashboard html file

--- apps/portal/next.config.mjs
+++ apps/portal/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/landing/:path*',
+                destination: '/landing/:path*',
+            },
+        ]
+    }
+};
 
 export default nextConfig;
